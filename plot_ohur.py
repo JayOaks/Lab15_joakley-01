58dabeb573e0abce590bd7b6cd27d73316659e83
Jeroldine Akuye Oakley <68546366+JayOaks@users.noreply.github.com>
2025-08-06 21:23:18 -0400

    Update requirements.txt and import required modules
    Update requirements.txt to include csv module for enumerating/reading the head.

---
+++ plot_ohur.py
@@ -0,0 +1,12 @@
+"""
+Ohio Unemployment Rate Visualization
+Jeroldine Oakley [author]
+This program reads unemployment data from OHRU.csv
+and plots it by year.
+08/06/2025
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import csv
